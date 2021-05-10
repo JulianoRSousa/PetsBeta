268e47ae8eece9bb6b78f093dd66b96be3f5cbfb
julianorodsousa <julianorodsousa@gmail.com>
2021-05-10 08:57:19 -0400

    Add LottieView imports

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   TouchableOpacity,
   View,
@@ -10,9 +10,41 @@ import {
 import * as AppColors from "../assets/Colors";
 
 export default function PetsTextInput(props) {
+  const ref_focusNow = useRef();
+  const noFocusYet = useRef();
+  const ref_me = useRef();
+
   useEffect(() => {});
   const width = Dimensions.get("window").width * 0.8;
   const height = Dimensions.get("window").height * 0.07;
+  const [nextFocusOld, setNextFocusOld] = useState('');
+  const [nextFocus, setNextFocus] = useState('');
+  const [name, setName] = useState('');
+  
+  useEffect(()=>{
+    if(props.name){
+      setName(props.name)
+    }
+    if(props.nextFocus){
+      setNextFocus(props.nextFocus)
+    }
+  },[])
+
+  function goFocus() {
+    console.log("nextFocus:",nextFocus)
+    console.log('name:',name)
+    console.log('nextFocusOld:',nextFocusOld)
+    setNextFocusOld(nextFocus);
+    console.log(ref_me.current.getKey())
+    ref_me.current.focus();
+console.log('focus??')
+    if (nextFocusOld == name) {
+      console.log('to no if')
+      setNextFocus(props.nextFocus());
+    }
+  }
+
+
   return (
     <View
       style={{
@@ -32,16 +64,19 @@ export default function PetsTextInput(props) {
           left: width / 8,
           top: -(height / 6),
           paddingHorizontal: 5,
-          fontSize:16
+          fontSize: 16,
         }}
       >
         {props.tittle}
       </Text>
       <TextInput
+      key={'pass'}
+        ref={ref_me}
         multiline={false}
         caretHidden={true}
         passwordRules={props.passwordRules}
         secureTextEntry={props.isPass}
+        onSubmitEditing={()=>goFocus(props.nextFocus)}
         style={{
           borderRadius: 30,
           marginHorizontal: width / 10,
--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -3,6 +3,7 @@ import { View, StatusBar, Text } from "react-native";
 import * as AppColors from "../../assets/Colors";
 import PetsButton from "../../components/Buttons";
 import PetsTextInput from "../../components/TextInput";
+import LottieView from "lottie-react-native";
 
 export default function login({ navigation }) {
   return (
@@ -22,17 +23,17 @@ export default function login({ navigation }) {
           fontSize: 70,
           height: "35%",
           width: "100%",
-          alignContent:'center',
-          alignItems:'center',
-          justifyContent:'center',
-          textAlign:'center',
-          textAlignVertical:'center'
+          alignContent: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+          textAlignVertical: "center",
         }}
       >
         Pets
       </Text>
-      <PetsTextInput tittle={"Usuário"} />
-      <PetsTextInput tittle={"Senha"} isPass={true}/>
+      <PetsTextInput name={'user'} nextFocus={'pass'} tittle={"Nome de usuário"}/>
+      <PetsTextInput name={'pass'} nextFocus={'user'} tittle={"Senha"} isPass={true} />
       <PetsButton
         tittle={"Entrar"}
         color={"blue"}
@@ -43,6 +44,22 @@ export default function login({ navigation }) {
         color={"light"}
         onPress={() => console.log("teste")}
       />
+      <View
+        style={{
+          height: "5%",
+          width: "35%",
+          alignItems: "center",
+          justifyContent:'center',
+          alignSelf:'baseline'
+        }}
+      >
+        <LottieView
+          source={require("../../assets/animations/loginHappyDog.json")}
+          autoSize={true}
+          autoPlay
+          loop
+        />
+      </View>
     </View>
   );
 }
